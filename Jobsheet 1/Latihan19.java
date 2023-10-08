77fe945073de1ddd390d9fe4b3ca77342d199048
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-10-08 20:16:37 +0700

    Jobsheet6_Kelompok

---
+++ Jobsheet 1/Latihan19.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan19
+ */
+public class Latihan19{
+
+    public static void main(String[] args) {
+        System.out.print ("Meisy Nadia Nababan");
+        //System.out.print("/nsaya adalah mahasiswa baru Juruan Teknologi Informasi-POLINEMA");
+        }
+}
