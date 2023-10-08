e9d192d7f77a2ce30c0880ae3fe670be1bdee1db
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-10-08 20:35:54 +0700

    Jobseet6_Kelompok

--- SistemGajiGuru.java
+++ SistemGajiGuru.java
@@ -1,6 +1,11 @@
+// Nama kelompok :
+// Devin I’zaz Radin D (2341760034)
+// Meisy Nadia Nababan (2341760031)
+// Rahmalia Mutia Farda (2341760130)
+
 import java.util.Scanner;
 
-public class PenggajianGuru {
+public class SistemGajiGuru {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -16,20 +21,29 @@ public class PenggajianGuru {
         System.out.print("Masukkan golongan (1 atau 2): ");
         int golongan = scanner.nextInt();
 
+        System.out.print("Masukkan jumlah jam kerja per bulan: ");
+        int torJamKerjaPerBulan = scanner.nextInt();
+
+        System.out.print("Masukkan jumlah hari masuk per bulan: ");
+        int totHariMasukPerBulan = scanner.nextInt();
+
+        System.out.print("Masukkan jumlah hari tidak masuk per bulan: ");
+        int totHariTidakMasukPerBulan = scanner.nextInt();
+
         double gajiPokok = 0;
         double pajak = 0;
 
-            if (jabatan.equalsIgnoreCase("PNS")) {
+        if (jabatan.equalsIgnoreCase("PNS")) {
             if (golongan == 1) {
                 gajiPokok = 4000000;
                 pajak = 0.005 * gajiPokok;  // 0.5% pajak
             } else if (golongan == 2) {
                 gajiPokok = 5000000;
-                pajak = 0.0015 * gajiPokok;  // 0.15% pajak
+                pajak = 0.0005 * gajiPokok;  // 0.15% pajak
             } else {
                 System.out.println("Golongan PNS yang Anda masukkan salah.");
             }
-            } else if (jabatan.equalsIgnoreCase("honorer")) {
+        } else if (jabatan.equalsIgnoreCase("honorer")) {
             if (golongan == 1) {
                 gajiPokok = 1000000;
                 pajak = 0.002 * gajiPokok;  // 0.2% pajak
@@ -38,10 +52,13 @@ public class PenggajianGuru {
                 pajak = 0.0015 * gajiPokok;  // 0.15% pajak
             } else {
                 System.out.println("Golongan honorer yang Anda masukkan salah.");
+            }
+        } else {
+            System.out.println("Jabatan yang Anda masukkan salah.");
         }
-            
+
         double gajiBersih = gajiPokok - pajak;
-          
+        
         System.out.println(" MENGHITUNG GAJI KARYAWAN ");
         System.out.println(" ======================== ");
         System.out.println("Nama: " + nama);
@@ -49,5 +66,5 @@ public class PenggajianGuru {
         System.out.println("Gaji Pokok: " + gajiPokok);
         System.out.println("Pajak: " + pajak);
         System.out.println("Gaji Bersih: " + gajiBersih);
-    }
+    } 
 }
