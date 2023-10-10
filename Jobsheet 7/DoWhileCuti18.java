1b4ab6aa8fafe7c6da2720849ca71527840b83cf
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-10-10 12:36:55 +0700

    Sebelum modifikasi

--- Jobsheet 7/DoWhileCuti18.java
+++ Jobsheet 7/DoWhileCuti18.java
@@ -1,9 +1,33 @@
 import java.util.Scanner;
-public class DoWhile18{
-    public static void main (String []args){
-        Scanner sc= new Scanner (System.in);
-        int jatahcuti, jumlahHari;
+
+public class DoWhileCuti18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jatahCuti,jumlahHari;
         String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")){
+                System.out.print("jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari<= jatahCuti){
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
+
 
     } 
 }
