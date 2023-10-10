345172b7e686450ed84d7341540590a307227c27
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-10-10 11:55:41 +0700

    sebelum modifikasi

--- Jobsheet 7/DoWhileCuti18.java
+++ Jobsheet 7/DoWhileCuti18.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class DoWhile18{
     public static void main (String []args){
-        Scanner input= 
-    }
+        Scanner sc= new Scanner (System.in);
+        int jatahcuti, jumlahHari;
+        String konfirmasi;
+
+    } 
 }
